2b85bf132bfaaa5bb53bf5b155fb75aeff9e17f4
autopp <autopp.inc@gmail.com>
2020-01-19 23:39:10 +0900

    feat: read magic

--- jvm.c
+++ jvm.c
@@ -2,14 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef FILE *reader_t;
-
 #define BUF_SIZE 1024
 #define error(...) do {\
   fprintf(stderr, __VA_ARGS__);\
   fputs("\n", stderr);\
   exit(1);\
 } while (1)
+
+typedef struct {
+  size_t size;
+  size_t cur;
+  char* data;
+} reader_t;
+
+void init_reader(reader_t *reader, size_t size, char *data) {
+  reader->size = size;
+  reader->cur = 0;
+  reader->data = data;
+}
+
+void read_bytes(reader_t *reader, unsigned char *buf, size_t n) {
+  if (reader->cur + n >= reader->size) {
+    error("cannot read from reader");
+  }
+  memcpy(buf, reader->data + reader->cur, n);
+  reader->cur += n;
+}
 
 int main(int argc, char **argv) {
   // load class file
@@ -41,6 +59,13 @@ int main(int argc, char **argv) {
   }
 
   fclose(fp);
+  reader_t class_reader;
+  init_reader(&class_reader, class_file_size, class_file_data);
+
+  // read magic
+  unsigned char magic[4];
+  read_bytes(&class_reader, magic, 4);
+  printf("%x%x%x%x\n", magic[0], magic[1], magic[2], magic[3]);
 
   return 0;
 }
